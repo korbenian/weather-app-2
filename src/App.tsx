f226ef50fbfe17e20d0cf06a3c0053eddafed4d0
Sergey <serzhkorobov11@gmail.com>
2025-04-23 19:32:42 +0300

    Первый коммит в новом репозитории

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+
+import WeatherSearch from './components/weather'
+
+function App () {
+  return (
+    <div className='App'>
+      <WeatherSearch />
+    </div>
+  )
+}
+
+export default App
